c81818390b1ca8fabf7529565ef8c15ea0ce95a5
dpozinen <dpozinen@gmail.com>
2019-09-18 18:09:05 +0300

    add custom Point class for clarity and no autoboxing

--- src/main/java/npuzzle/logic/Evaluator.java
+++ src/main/java/npuzzle/logic/Evaluator.java
@@ -1,16 +1,13 @@
 package npuzzle.logic;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
+import static npuzzle.utils.Constants.*;
 
 import java.util.*;
 
-import static npuzzle.utils.Constants.*;
-
 class Evaluator {
 
-	private static final Map<Integer, List<Pair<Integer, Integer>>> xyListMap = new HashMap<>();
-    private static final Map<Integer, State> finalStateMap = new HashMap<>();
+	private static final Map<Integer, List<Point>> xyListMap     = new HashMap<>();
+    private static final Map<Integer, State>       finalStateMap = new HashMap<>();
 
 	@FunctionalInterface
 	public interface Heuristic {
@@ -18,7 +15,7 @@ class Evaluator {
 	}
 
 	private static int manhattan(State state, int n) {
-		List<Pair<Integer, Integer>> xyList = xyListMap.get(n);
+		List<Point> xyList = xyListMap.get(n);
 		int[] tiles = state.getTiles();
 		int tile;
 		int x, y;
@@ -27,13 +24,13 @@ class Evaluator {
 		for (int index = 0; index < tiles.length; index++) {
 			x = index / n; y = index % n;
 			tile = tiles[index];
-			stateEval += Math.abs(x - xyList.get(tile).getKey()) + Math.abs(y - xyList.get(tile).getValue());
+			stateEval += Math.abs(x - xyList.get(tile).x) + Math.abs(y - xyList.get(tile).y);
 		}
 		return stateEval;
 	}
 
 	private static int euclidean(State state, int n) {
-		List<Pair<Integer, Integer>> xyList = xyListMap.get(n);
+		List<Point> xyList = xyListMap.get(n);
 		int[] tiles = state.getTiles();
 		int tile;
 		int x, y;
@@ -42,7 +39,7 @@ class Evaluator {
 		for ( int index = 0; index < tiles.length; index++) {
 			x = index / n; y = index % n;
 			tile = tiles[index];
-			stateEval += Math.sqrt(Math.pow(x - xyList.get(tile).getKey(), 2) + Math.pow(y - xyList.get(tile).getValue(), 2));
+			stateEval += Math.sqrt(Math.pow(x - xyList.get(tile).x, 2) + Math.pow(y - xyList.get(tile).y, 2));
 		}
 		return stateEval;
 	}
@@ -75,10 +72,10 @@ class Evaluator {
 		State finalState = State.createFinal(n);
 		finalStateMap.put(n, finalState);
 		int[] finalOrder = finalState.getTiles();
-		List<Pair<Integer, Integer>> xyList = new ArrayList<>(Collections.nCopies(finalOrder.length, null));
+		List<Point> xyList = new ArrayList<>(Collections.nCopies(finalOrder.length, null));
 
 		for ( int i = 0; i < finalOrder.length; i++)
-			xyList.set(finalOrder[i], new ImmutablePair<>(i / n, i % n));
+			xyList.set(finalOrder[i], new Point(i / n, i % n));
 
 		xyListMap.put(n, Collections.unmodifiableList(xyList));
 	}
@@ -86,5 +83,14 @@ class Evaluator {
 	static State getFinal(int n) {
 	    return finalStateMap.get(n);
     }
+
+	private static class Point {
+		final int x;
+		final int y;
+
+		Point(int x, int y) {
+			this.x = x; this.y = y;
+		}
+	}
 
 }
